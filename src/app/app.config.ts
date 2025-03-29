e62fd5bf1d5590e207e1b9c1d38d530536438113
Rodrigo Meireles <meireles.rsilva22@gmail.com>
2025-03-29 12:45:46 -0300

    integracao com o firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,28 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+// Import the functions you need from the SDKs you need
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDGwIk2uhf4ddyJ_1u8LB9jPmeEOWQuNXU",
+  authDomain: "isa-variedades.firebaseapp.com",
+  projectId: "isa-variedades",
+  storageBucket: "isa-variedades.firebasestorage.app",
+  messagingSenderId: "138204153544",
+  appId: "1:138204153544:web:f0901fa45dc9ffd1e487fd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+      provideFirestore(() => getFirestore()),
+  ]
 };

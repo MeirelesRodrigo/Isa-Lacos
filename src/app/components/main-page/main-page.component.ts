825a845e0ff36ec769130d2e40f96868286e5e87
Rodrigo Meireles <meireles.rsilva22@gmail.com>
2025-04-07 20:20:16 -0300

    Criando logomarca e aidiconando cores

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
 
 
 @Component({
   selector: 'app-main-page',
-  imports:[],
+  imports: [HeaderComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
